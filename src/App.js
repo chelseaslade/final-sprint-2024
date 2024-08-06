e9d118dff46634fef5df2dafe5271be28002715d
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-08-06 14:20:12 -0230

    Added Router with page links (framework only)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@
 //Final Sprint August 2024
 
 import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.js";
+import Layout from "./pages/Layout.js";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/products" element={<Home />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
