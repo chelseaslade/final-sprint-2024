eaccc938b5837dc0349247d287c286111e01de52
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-08-08 14:13:22 -0230

    Started confirmation page

---
+++ src/pages/ConfirmationPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const ConfirmationPage = () => {
+  return (
+    <>
+      <div className="ConfirmationPage"></div>
+    </>
+  );
+};
+
+export default ConfirmationPage;
