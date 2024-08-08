66d9701e377d11038ce584d50499abf10c43579b
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-08-08 14:08:06 -0230

    Contact us page almost done

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,24 +1,30 @@
 import React from "react";
+import storePic from "../pics/storePic.jpg";
 
 const Contact = () => {
   return (
     <>
       <div className="ContactPage">
         <div className="ContactDetails">
-          <h2>Contact Us</h2>
-          <h4>Address:</h4>
-          <p id="Address">
-            McCarthy & Slade Publishing House <br />
-            16 Silverwood Drive <br />
-            St. John's, NL <br /> A1A 1A1
-          </p>
-          <h4>Contact:</h4>
-          <p>
-            Phone: (709) 907-7979 <br /> Fax: (709) 907-9797 <br />
-            Email: mccarthyslade@publishing.com
-          </p>
-          <h4>Hours of Operation: </h4>
-          <p>Mon - Fri: 9AM - 5PM</p>
+          <div className="ContactText">
+            <h2>Contact Us</h2>
+            <h4>Address:</h4>
+            <p id="Address">
+              McCarthy & Slade Publishing House <br />
+              16 Silverwood Drive <br />
+              St. John's, NL <br /> A1A 1A1
+            </p>
+            <h4>Contact:</h4>
+            <p>
+              Phone: (709) 907-7979 <br /> Fax: (709) 907-9797 <br />
+              Email: mccarthyslade@publishing.com
+            </p>
+            <h4>Hours of Operation: </h4>
+            <p>Mon - Fri: 9AM - 5PM</p>
+          </div>
+          <div className="PicContainer">
+            <img src={storePic} alt="bookstore" id="StorePicture"></img>
+          </div>
         </div>
       </div>
     </>
