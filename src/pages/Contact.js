571dc2d8c1e41beffdd5df0c5f728f6ef9f4b70c
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-08-08 13:37:03 -0230

    Contact page updated

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Contact = () => {
-  return <h1>Contact</h1>;
+  return (
+    <>
+      <div className="ContactPage">
+        <div className="ContactDetails">
+          <h2>Contact Us</h2>
+          <h4>Address:</h4>
+          <p id="Address">
+            McCarthy & Slade Publishing House <br />
+            16 Silverwood Drive <br />
+            St. John's, NL <br /> A1A 1A1
+          </p>
+          <h4>Contact:</h4>
+          <p>
+            Phone: (709) 907-7979 <br /> Fax: (709) 907-9797 <br />
+            Email: mccarthyslade@publishing.com
+          </p>
+          <h4>Hours of Operation: </h4>
+          <p>Mon - Fri: 9AM - 5PM</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
