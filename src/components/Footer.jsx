be6a743f582113e8b5fcafdb5e264e07133f2b96
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-08-09 11:53:41 -0230

    Added social media links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 import "../App.css";
+import Facebook from "../pics/Facebook.png";
+import Insta from "../pics/Insta.png";
+import LinkedIn from "../pics/LinkedIn.png";
 
 const Footer = () => {
   return (
     <>
-      <div className="Footer"></div>
+      <div className="Footer">
+        <div className="SocialMedia">
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Facebook} alt="Facebook Icon"></img>
+          </a>
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Insta} alt="Instagram Icon"></img>
+          </a>
+          <a
+            href="https://www.linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={LinkedIn} alt="Instagram Icon"></img>
+          </a>
+        </div>
+      </div>
     </>
   );
 };
